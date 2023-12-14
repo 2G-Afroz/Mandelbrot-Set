a7dbb54d0105392bd586f8aaa7f3a83ed1375592
mdafrozalam <siddiquiafroz15@gmail.com>
2023-12-14 19:06:28 +0530

    Basic visualization of mandelbrot set

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 debug: test
 	@builds/MandelbrotSet
 test: src/*.cpp
-	@g++ src/*.cpp -o builds/MandelbrotSet -lraylib
+	@g++ src/*.cpp -o builds/MandelbrotSet -L./lib -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -std=c++11
 clean:
 	rm builds/* -r
 	rm temp -r
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,64 @@
 #include<raylib.h>
+#include<iostream>
+using namespace std;
+
+void mandelFunction(vector<Vector2> &pos, float x, float y){
+				// Imaginary num "x + iy"
+
+				for(int i=0;i<20;i++){
+								pos[i].x = x;
+								pos[i].y = y;
+
+								// temp
+								float a = x*x - y*y;
+								float b = 2*x*y;
+								x = a;
+								y = b;
+				}
+}
+
+void printArrVal(vector<float> pos){
+				for(int i=0;i<20;i++){
+								DrawText(TextFormat("%f", pos[i]), 10, 40+20*i, 20, WHITE);
+				}
+}
 
 int main(){
-	int windowWidth = 700;
-	int windowHeight = 700; 
+	int windowWidth = 1200;
+	int windowHeight = 850; 
 	InitWindow(windowWidth, windowHeight,"MandelbrotSet");
 
-	ClearBackground(BLACK);
+	Vector2 centre = {600,425};
+	vector<Vector2> pos(20);
 
 	while(!WindowShouldClose()){
+    ClearBackground(BLACK);
 		BeginDrawing();
+				DrawCircle(centre.x, centre.y, 1,RED);
+				// Mouse Work
+				float mouseX = (GetMouseX()-centre.x)/200.0f;
+				float mouseY = (centre.y-GetMouseY())/200.0f;
+				
+				DrawText(TextFormat("MouseX: %f", mouseX), 10, 10, 30, WHITE); 
+				DrawText(TextFormat("MouseY: %f", mouseY), 10, 40, 30, WHITE); 
+				// Mandel Function
+				mandelFunction(pos, mouseX, mouseY);
+
+				// Drawing circle border
+				DrawCircleLines(centre.x, centre.y, 200, RED);
+
+				// Draw line
+				for(int i=0;i<19;i++){
+								DrawLine(centre.x+pos[i].x*200, centre.y-pos[i].y*200,
+																centre.x+pos[i+1].x*200, centre.y-pos[i+1].y*200,
+																WHITE); 
+				}
+								
+
+				// Drawing circles
+				for(int i=0;i<20;i++){
+								DrawCircle(centre.x + pos[i].x*200, centre.y-pos[i].y*200, 4, GREEN);
+				}
 		EndDrawing();
 	}
 
