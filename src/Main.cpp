2af31b72e0ef058ceae46349a71f5c313204c0a8
mdafrozalam <siddiquiafroz15@gmail.com>
2023-12-16 20:47:43 +0530

    Added zoom option.

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,12 +10,15 @@ float map(float value, float fromMin, float fromMax, float toMin, float toMax) {
 }
 
 int main(){
-	int windowWidth = 1320;
-	int windowHeight = 880; 
+	int windowWidth = 1335;
+	int windowHeight = 890; 
 	InitWindow(windowWidth, windowHeight,"MandelbrotSet");
+
+	float zoom = 1.0f;
 
 	while(!WindowShouldClose()){
   	ClearBackground(WHITE);
+
 		BeginDrawing();
 			// Drawing each pixel
 			for(int i=0;i<windowWidth;i++){
@@ -25,31 +28,32 @@ int main(){
 					float x = 0;		// real part
 					float y = 0;		// imaginary part
 					// This is value of c
-					float a = map(i, 0, windowWidth, -2, 1); // real part
-          float b = map(j, 0, windowHeight, -1, 1);	// imaginary part
+					float a = map(i, 0, windowWidth, -2*zoom, 1*zoom); // real part
+          float b = map(j, 0, windowHeight, -1*zoom, 1*zoom);	// imaginary part
 
           int n = 0;
 
-          while(n<256){
+          while(n<255){
 						float xx = x*x-y*y+a;
 						float yy = 2*x*y+b;
 
 						x = xx;
 						y = yy;
 
-						if(abs(x + y) > 100.0f){
+						if(abs(x + y) >= 10.0f){
 							break;
 						}
 						n++;
           }
 					// n = map(n, 0, 100, 0, 256); // this mapping gives different border effect.
 					// Coloring Pixels
+
 					Color pixelColor = {(unsigned char)n, (unsigned char)n, (unsigned char)n, 255};
 					DrawPixel(i,j,pixelColor);
 
+				}
 			}
-			}
-		EndDrawing();
+			EndDrawing();
 	}
 
 	CloseWindow();
